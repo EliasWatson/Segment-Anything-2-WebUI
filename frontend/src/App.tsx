dc5e0129721d6320ae5fc24fa606608f2b5edf6d
Elias Watson <me@eliaswatson.dev>
2024-08-11 17:02:11 -0400

    wip: ThreeJS viewport & image loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,55 +1,122 @@
-import { type ReactNode, useCallback, useMemo, useRef, useState } from "react";
-import { useImageUpload } from "./queries/use-image-upload.ts";
-import { HintPoint } from "./types.ts";
-import { useImageSegment } from "./queries/use-image-segment.ts";
-import { apiUrl } from "./queries/util.ts";
+import {
+  type ReactNode,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+import { MapControls } from "@react-three/drei";
+import { useDropzone } from "react-dropzone";
+
+const initialCameraZoom = Math.min(window.innerWidth, window.innerHeight) * 0.8;
 
 function App(): ReactNode {
   const [imageUrl, setImageUrl] = useState<string | undefined>(undefined);
-
-  const imageRef = useRef<HTMLImageElement | null>(null);
-  const imageElement = imageRef.current;
-
-  const [hintPoints, setHintPoints] = useState<HintPoint[]>([]);
-  const [selectedPoint, setSelectedPoint] = useState<number | undefined>(
+  const [imageTexture, setImageTexture] = useState<THREE.Texture | undefined>(
     undefined,
   );
 
-  const { data: imageIdResponse, mutate: uploadImage } = useImageUpload();
-  const imageId = imageIdResponse?.data;
+  useEffect(() => {
+    if (imageUrl === undefined) return;
 
-  const setImage = useCallback(
-    (image: Blob | undefined) => {
-      if (imageUrl !== undefined) {
-        URL.revokeObjectURL(imageUrl);
-      }
+    new THREE.TextureLoader().loadAsync(imageUrl).then((texture) => {
+      setImageTexture(texture);
+    });
+  }, [imageUrl]);
 
-      setHintPoints([]);
+  // const imageRef = useRef<HTMLImageElement | null>(null);
+  // const imageElement = imageRef.current;
+  //
+  // const [hintPoints, setHintPoints] = useState<HintPoint[]>([]);
+  // const [selectedPoint, setSelectedPoint] = useState<number | undefined>(
+  //   undefined,
+  // );
+  //
+  // const { data: imageIdResponse, mutate: uploadImage } = useImageUpload();
+  // const imageId = imageIdResponse?.data;
+  //
+  // const setImage = useCallback(
+  //   (image: Blob | undefined) => {
+  //     if (imageUrl !== undefined) {
+  //       URL.revokeObjectURL(imageUrl);
+  //     }
+  //
+  //     setHintPoints([]);
+  //
+  //     if (image === undefined) {
+  //       setImageUrl(undefined);
+  //       return;
+  //     }
+  //
+  //     uploadImage(image);
+  //     setImageUrl(URL.createObjectURL(image));
+  //   },
+  //   [imageUrl, uploadImage],
+  // );
+  //
+  // const { data: maskIdResponse, mutate: segmentImage } = useImageSegment();
+  // const maskId = maskIdResponse?.data.at(0);
+  //
+  // const maskUrl = useMemo(() => {
+  //   if (imageId === undefined || maskId === undefined) {
+  //     return undefined;
+  //   }
+  //
+  //   return `${apiUrl}/api/image/get_mask/${imageId}/${maskId}`;
+  // }, [imageId, maskId]);
 
-      if (image === undefined) {
-        setImageUrl(undefined);
-        return;
-      }
-
-      uploadImage(image);
-      setImageUrl(URL.createObjectURL(image));
-    },
-    [imageUrl, uploadImage],
-  );
-
-  const { data: maskIdResponse, mutate: segmentImage } = useImageSegment();
-  const maskId = maskIdResponse?.data.at(0);
-
-  const maskUrl = useMemo(() => {
-    if (imageId === undefined || maskId === undefined) {
-      return undefined;
+  const planeDimensions = useMemo((): [number, number] => {
+    if (imageTexture === undefined) {
+      return [1, 1];
     }
 
-    return `${apiUrl}/api/image/get_mask/${imageId}/${maskId}`;
-  }, [imageId, maskId]);
+    const imageWidth = imageTexture.image.width;
+    const imageHeight = imageTexture.image.height;
+    return imageWidth > imageHeight
+      ? [1, imageHeight / imageWidth]
+      : [imageWidth / imageHeight, 1];
+  }, [imageTexture]);
 
-  return <Canvas></Canvas>;
+  const onDrop = useCallback((acceptedFiles: File[]): void => {
+    if (acceptedFiles.length > 0) {
+      setImageUrl((imageUrl) => {
+        if (imageUrl !== undefined) {
+          URL.revokeObjectURL(imageUrl);
+        }
+
+        return URL.createObjectURL(acceptedFiles[0]);
+      });
+    }
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    noClick: true,
+    accept: { "image/*": [] },
+  });
+
+  return (
+    <div className="w-full h-full" {...getRootProps()}>
+      <input {...getInputProps()} />
+      <Canvas
+        frameloop="demand"
+        orthographic
+        camera={{
+          position: [0, 0, 50],
+          zoom: initialCameraZoom,
+          up: [0, 0, 1],
+          far: 10000,
+        }}
+      >
+        <mesh>
+          <planeGeometry args={planeDimensions} />
+          {imageTexture && <meshBasicMaterial map={imageTexture} />}
+        </mesh>
+        <MapControls enableRotate={false} />
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
